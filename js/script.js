f2d6073e38943aed03f8eabba738331354b5cd45
yusufroqib <114329528+yusufroqib@users.noreply.github.com>
2023-10-08 20:55:57 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
+
 btnNavEl.addEventListener("click", function() {
     headerEl.classList.toggle("nav-open")
 });
